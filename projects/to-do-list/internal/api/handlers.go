8da550bef042d2d61c73ff43191bf64ddbe045c7
araujo <theriancirculo@protonmail.com>
2025-08-10 10:41:28 -0300

    feat(api): Implement to-do list API

--- projects/to-do-list/internal/api/handlers.go
+++ projects/to-do-list/internal/api/handlers.go
@@ -1,9 +1,66 @@
 package api
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
-func TodoHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Server) getListTasksHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
+		return
+	}
+
+	tasks := s.task.GetListTasks()
+
+	jsonResponse, err := json.Marshal(tasks)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "Rota de tarefas To-Do!"}`))
+	w.Write(jsonResponse)
+}
+
+func (s *Server) addTaskHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var requestPayload struct {
+		Description string `json:"description"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&requestPayload)
+	if err != nil {
+		http.Error(w, "Corpo da requisição inválido ou mal formatado", http.StatusBadRequest)
+		return
+	}
+
+	if requestPayload.Description == "" {
+		http.Error(w, "O campo 'description' não pode ser vazio", http.StatusBadRequest)
+		return
+	}
+
+	newTask := s.task.AddTask(requestPayload.Description)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newTask)
+}
+
+func (s *Server) deleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+func (s *Server) updateTaskHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+func (s *Server) getTaskHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+func (s *Server) checkTaskHandler(w http.ResponseWriter, r *http.Request) {
+
 }
--- projects/to-do-list/internal/api/server.go
+++ projects/to-do-list/internal/api/server.go
@@ -2,22 +2,31 @@ package api
 
 import (
 	"net/http"
+
+	"github.com/dev-araujo/golang__sandbox/to-do-list/internal/task"
 )
 
 type Server struct {
 	router *http.ServeMux
+	task   task.Service
 }
 
-func NewServer() *Server {
+func NewServer(task task.Service) *Server {
 	server := &Server{
 		router: http.NewServeMux(),
+		task:   task,
 	}
 	server.registerRoutes()
 	return server
 }
 
 func (s *Server) registerRoutes() {
-	s.router.HandleFunc("/to-do", TodoHandler)
+	s.router.HandleFunc("/todo/list", s.getListTasksHandler)
+	s.router.HandleFunc("/todo/add", s.addTaskHandler)
+	s.router.HandleFunc("/todo/delete", s.deleteTaskHandler)
+	s.router.HandleFunc("/todo/update", s.updateTaskHandler)
+	s.router.HandleFunc("/todo/get", s.getTaskHandler)
+	s.router.HandleFunc("/todo/check", s.checkTaskHandler)
 }
 
 func (s *Server) Router() http.Handler {
